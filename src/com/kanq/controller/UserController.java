48b9c2c4d44e833e71a58d197d9bfc4b6998ecbc
x1342 <1783198046@qq.com>
2018-01-27 19:08:50 +0800

    修改一下

--- src/com/kanq/controller/UserController.java
+++ src/com/kanq/controller/UserController.java
@@ -18,15 +18,6 @@ public class UserController {
     }
 
 
-    @RequestMapping("usersss")
-    @ResponseBody
-    private  App_assetfilesex Userssss(){
-        App_assetfilesex app_assetfilesex = new App_assetfilesex();
-        app_assetfilesex.setPath("a11");
-        app_assetfilesex.setAssetId("2212");
-        app_assetfilesex.setFileName("啥的");
-        app_assetfilesex.setFileType("jpg");
-        return app_assetfilesex;
-    }
+
 
 }
